8bf7e7caf7b4c4499d502ce5007f6dae2c25d4e1
mikalai2006 <composer.opencart@gmail.com>
2025-02-13 13:55:06 +0300

    feat: change icon

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export default {
   name: "Кингвуд",
   slug: "kingwood-app-rn",
-  version: "1.0.4",
+  version: "1.0.5",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "kingwood",

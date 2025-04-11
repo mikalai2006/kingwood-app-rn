e9cba4adafa4ac2780c6d5dfdb67b97726d1a792
mikalai2006 <composer.opencart@gmail.com>
2025-04-11 08:17:45 +0300

    fix: hidden oklad

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -72,8 +72,7 @@ export default function TabProfileScreen() {
   const onShowOklad = () => {
     Alert.alert(
       t("oklad"),
-      `${userFromStore?.oklad}
-    ${getNoun(userFromStore?.oklad, "₽", "₽", "₽")}`,
+      `${userFromStore?.oklad} ${getNoun(userFromStore?.oklad, "₽", "₽", "₽")}`,
       [
         // {
         //   text: t("button.no"),

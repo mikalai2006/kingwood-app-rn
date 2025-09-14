04288f55d95862132a4583cfb164a09710875817
mikalai2006 <composer.opencart@gmail.com>
2025-09-14 19:08:40 +0300

    feat: add options localhost emulator

--- utils/global.ts
+++ utils/global.ts
@@ -5,6 +5,10 @@ export const isWriteConsole = true;
 // export const hostSERVER = "https://kingwood-apps.ru";
 // export const wsAPI = "wss://kingwood-apps.ru/api/v1/ws/room1";
 
-export const hostAPI = "http://localhost:8000/api/v1";
-export const hostSERVER = "http://localhost:8000";
-export const wsAPI = "ws://localhost:8000/api/v1/ws/room1";
+// export const hostAPI = "http://localhost:8000/api/v1";
+// export const hostSERVER = "http://localhost:8000";
+// export const wsAPI = "ws://localhost:8000/api/v1/ws/room1";
+
+export const hostAPI = "http://10.0.2.2:8000/api/v1";
+export const hostSERVER = "http://10.0.2.2:8000";
+export const wsAPI = "ws://10.0.2.2:8000/api/v1/ws/room1";

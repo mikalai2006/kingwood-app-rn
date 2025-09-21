4679ed37aba871332a878582c5b6dd4e04d5b75b
mikalai2006 <composer.opencart@gmail.com>
2025-09-21 19:28:23 +0300

    feat: change global environments

--- utils/global.ts
+++ utils/global.ts
@@ -1,13 +1,21 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+
 export const timeoutMaxRequest = 10000; // in milliseconds
-export const isWriteConsole = true;
+export const isWriteConsole = IS_DEV;
 
 // export const hostAPI = "https://kingwood-apps.ru/api/v1";
 // export const hostSERVER = "https://kingwood-apps.ru";
 // export const wsAPI = "wss://kingwood-apps.ru/api/v1/ws/room1";
 
-export const hostAPI = "http://localhost:8000/api/v1";
-export const hostSERVER = "http://localhost:8000";
-export const wsAPI = "ws://localhost:8000/api/v1/ws/room1";
+export const hostAPI = IS_DEV
+  ? "http://localhost:8000/api/v1"
+  : "https://kingwood-apps.ru/api/v1";
+export const hostSERVER = IS_DEV
+  ? "http://localhost:8000"
+  : "https://kingwood-apps.ru";
+export const wsAPI = IS_DEV
+  ? "ws://localhost:8000/api/v1/ws/room1"
+  : "wss://kingwood-apps.ru/api/v1/ws/room1";
 
 // Emulator.
 // export const hostAPI = "http://10.0.2.2:8000/api/v1";
